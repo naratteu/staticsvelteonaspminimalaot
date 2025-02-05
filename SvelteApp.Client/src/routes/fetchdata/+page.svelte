87750d9a94c006d20325c864c509ae7cac503df3
박성원 <skfhddlg@naver.com>
2025-02-05 11:04:24 +0900

    dotnet new svelte -n SvelteApp # from : https://github.com/Quickz/svelte-dotnet-template/tree/184a1ff

---
+++ SvelteApp.Client/src/routes/fetchdata/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import PageTitle from "../components/PageTitle.svelte";
+    import { onMount } from "svelte";
+    import { Api, type WeatherForecast } from "../../Api";
+    import { config } from "../../config";
+
+    let forecasts : Array<WeatherForecast> = [];
+
+    onMount(async () =>
+    {
+        const api = new Api();
+        api.baseUrl = config.apiEndpoint;
+        
+        const response = await api.weatherForecast.weatherForecastList();
+        
+        if (response.status == 200)
+        {
+            forecasts = response.data;
+        }
+    });
+</script>
+
+<PageTitle>Weather forecast</PageTitle>
+
+<h1>Weather forecast</h1>
+
+<p>This component demonstrates fetching data from the server.</p>
+
+{#if forecasts == null}
+    <p><em>Loading...</em></p>
+{:else}
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Temp. (C)</th>
+                <th>Temp. (F)</th>
+                <th>Summary</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each forecasts as forecast}
+                <tr>
+                    <td>{new Date(forecast.date ?? "").toLocaleDateString()}</td>
+                    <td>{forecast.temperatureC}</td>
+                    <td>{forecast.temperatureF}</td>
+                    <td>{forecast.summary}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+{/if}
